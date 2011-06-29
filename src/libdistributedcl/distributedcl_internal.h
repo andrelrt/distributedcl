6889fec2d92eeb3a34ab9073717872d70b11bf02
andrelrt <andrelrt@cb344d40-e590-48b6-843f-7a98a4fc1ff3>
2011-06-29 21:44:06 +0000

    git-svn-id: http://svn.code.sf.net/p/distributedcl/code/trunk@137 cb344d40-e590-48b6-843f-7a98a4fc1ff3

--- src/libdistributedcl/distributedcl_internal.h
+++ src/libdistributedcl/distributedcl_internal.h
@@ -50,6 +50,68 @@ namespace dcl {
 typedef uint16_t remote_id_t;
 typedef std::vector< dcl::info::generic_device* > devices_t;
 //-----------------------------------------------------------------------------
+static const union { long one; char little; } is_endian = {1};
+//-----------------------------------------------------------------------------
+static inline uint16_t host_to_network( uint16_t value )
+{
+    if( is_endian.little )
+    {
+        return value;
+    }
+    else
+    {
+        return (value >> 8) | (value << 8);
+    }
+}
+//-----------------------------------------------------------------------------
+static inline uint32_t host_to_network( uint32_t value )
+{
+    if( is_endian.little )
+    {
+        return value;
+    }
+    else
+    {
+        return ((value << 24) | (value >> 24)) |
+               ((value << 8) & 0x00ff0000) |
+               ((value >> 8) & 0x0000ff00);
+    }
+}
+//-----------------------------------------------------------------------------
+static inline uint64_t host_to_network( uint64_t value )
+{
+    if( is_endian.little )
+    {
+        return value;
+    }
+    else
+    {
+        return ((value << 56) | (value >> 56)) |
+               ((value << 40) & 0x00ff000000000000) |
+               ((value << 24) & 0x0000ff0000000000) |
+               ((value << 8)  & 0x000000ff00000000) |
+               ((value >> 8)  & 0x00000000ff000000) |
+               ((value >> 24) & 0x0000000000ff0000) |
+               ((value >> 40) & 0x000000000000ff00);
+    }
+}
+//-----------------------------------------------------------------------------
+static inline uint16_t network_to_host( uint16_t value )
+{
+    return host_to_network( value );
+}
+//-----------------------------------------------------------------------------
+static inline uint32_t network_to_host( uint32_t value )
+{
+    return host_to_network( value );
+}
+//-----------------------------------------------------------------------------
+static inline uint64_t network_to_host( uint64_t value )
+{
+    return host_to_network( value );
+}
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 } // namespace dcl
 //-----------------------------------------------------------------------------
 #endif // _DCL_INTERNAL_H_
