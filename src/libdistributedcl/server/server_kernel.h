e7fe79950ff99e3e330e7ad61ae447a134078023
andrelrt <andrelrt@cb344d40-e590-48b6-843f-7a98a4fc1ff3>
2012-06-26 19:40:35 +0000

    git-svn-id: http://svn.code.sf.net/p/distributedcl/code/trunk@276 cb344d40-e590-48b6-843f-7a98a4fc1ff3

--- src/libdistributedcl/server/server_kernel.h
+++ src/libdistributedcl/server/server_kernel.h
@@ -66,18 +66,15 @@ class msgSetKernelArg_command :
 {
 public:
     msgSetKernelArg_command( message_sp_t message_ptr, dcl::network::server::server_session_context* session_context_ptr ) :
-        async_server_command< dcl::network::message::msgSetKernelArg >( message_ptr, session_context_ptr )
-    {
-        server_platform& server = session_context_ptr_->get_server_platform();
-
-        dcl::composite::composite_command_queue* queue_ptr = 
-            server.get_command_queue_manager().get( message_->get_command_queue_id() );
-
-        set_command_queue( queue_ptr );
-    }
+        async_server_command< dcl::network::message::msgSetKernelArg >( message_ptr, session_context_ptr ){}
 
     void execute();
     virtual bool async_run() const;
+
+    inline void set_queue( dcl::composite::composite_command_queue* queue_ptr )
+    {
+        set_command_queue( queue_ptr );
+    }
 };
 //-----------------------------------------------------------------------------
 class msgGetKernelWorkGroupInfo_command :
